b0e212aa8018c0d9e70c17d19a32790a289f784e
이정제 <jjj030109@gmail.com>
2025-06-25 09:43:04 +0900

    헤더 끝

---
+++ taekwon-frontend/src/pages/introductionPages/DirectionsMap.jsx
@@ -0,0 +1,14 @@
+import { useEffect } from "react";
+
+const DirectionsMap = () => {
+
+  return (
+    <div className="flex flex-col min-h-screen">
+      <main className="flex-grow container mx-auto p-4 mt-20">
+        <p>찾아오시는 길</p>
+      </main>
+    </div>
+  );
+}
+
+export default DirectionsMap;
